7196295f66176f963b7866aa40a8948d816b9624
Manmohan Singh <97243552+manmohan-singh-1302@users.noreply.github.com>
2023-04-14 14:18:47 +0530

    first commit

---
+++ backend/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const connectDataBase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      //   useNewunifiedParser: true,
+      //   UseCreateIndex: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log(`Mongodb Connected with server: ${data.connection.host}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = connectDataBase;
